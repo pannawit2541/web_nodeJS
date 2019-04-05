5586074e36b116fa35818b828e3ec858c22115ee
pannawit2541 <pannawit_panwong@cmu.ac.th>
2019-04-05 23:09:39 +0700

    edit commit

--- node.js
+++ node.js
@@ -15,7 +15,7 @@ const path = require('path');
 const server = http.createServer((req, res) => {
     let filePath = path.join(
         __dirname,
-        'public',
+        'main',
         req.url === '/' ? 'index.html' : req.url
     );
 
@@ -50,34 +50,34 @@ const server = http.createServer((req, res) => {
     // Read file
 
     if (req.url === '/' || req.url === '/index.html') {
-        fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
+        fs.readFile(path.join(__dirname, 'main', 'index.html'), (err, content) => {
             if (err) throw err;
             res.writeHead(200, { 'content-type': contentType });
             res.end(content);
 
         })
     } else if (req.url === '/contact' || req.url === '/contact.html') {
-        fs.readFile(path.join(__dirname, 'public', 'contact.html'), (err, content) => {
+        fs.readFile(path.join(__dirname, 'main', 'contact.html'), (err, content) => {
             if (err) throw err;
             res.writeHead(200, { 'content-type': contentType });
             res.end(content);
         })
     } else if (req.url === '/gallery' || req.url === '/gallery.html') {
-        fs.readFile(path.join(__dirname, 'public', 'gallery.html'), (err, content) => {
+        fs.readFile(path.join(__dirname, 'main', 'gallery.html'), (err, content) => {
             if (err) throw err;
             res.writeHead(200, { 'content-type': contentType });
             res.end(content);
         })
     } else if (req.url.split('/')[1] === 'assets' && req.url.split('/')[2] === 'css') {
         let file = req.url.split('/')[3];
-        fs.readFile(path.join(__dirname, 'public', 'assets', 'css', file ), (err, content) => {
+        fs.readFile(path.join(__dirname, 'main', 'assets', 'css', file ), (err, content) => {
             if (err) throw err;
             res.writeHead(200, { 'content-type': contentType });
             res.end(content);
         })
     } else if (req.url.split('/')[1] === 'images') {
         let file = req.url.split('/')[2]
-        fs.readFile(path.join(__dirname, 'public', 'images', file), (err, content) => {
+        fs.readFile(path.join(__dirname, 'main', 'images', file), (err, content) => {
             if (err) throw err;
             res.writeHead(200, { 'content-type': contentType });
             res.end(content);
@@ -85,14 +85,14 @@ const server = http.createServer((req, res) => {
 
     } else if (req.url.split('/')[1] === 'assets' && req.url.split('/')[2] === 'js') {
         let file = req.url.split('/')[3];
-        fs.readFile(path.join(__dirname, 'public', 'assets', 'js', file), (err, content) => {
+        fs.readFile(path.join(__dirname, 'main', 'assets', 'js', file), (err, content) => {
             if (err) throw err;
             res.writeHead(200, { 'content-type': contentType });
             res.end(content);
         })
 
     } else {
-        fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
+        fs.readFile(path.join(__dirname, 'main', '404.html'), (err, content) => {
             if (err) throw err;
             res.writeHead(404, { 'content-type': 'text/html' });
             res.end('<h1>This is index.html</h1>');
